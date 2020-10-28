8e8d097b77cc0c8e5ffa16624016fdbc48aa6446
Yuya Sasaki <yuya.sasaki@2fc-group.jp>
2020-10-28 13:56:00 +0900

    処理を共通化

--- checker_sanshain.py
+++ checker_sanshain.py
@@ -1,20 +1,6 @@
-import time
-import datetime
+from checker import mugen
 
-import requests
-import simpleaudio
-
-base_url = "https://epark.jp/receive_department/wait/reserve_minute_select_ajax/172441/depart/21429?receive%5Breceipt_date%5D%5Bdate%5D=1603810800&receive%5Breceipt_date%5D%5Bhour%5D="
-hours = [18, 19, 20]
-
-while True:
-    for hour in hours:
-        res = requests.get(base_url + str(hour))
-        if "～" in res.text:
-            print(f"{hour}発見！")
-            wav_obj = simpleaudio.WaveObject.from_wave_file("発見の音.wav")
-            play_obj = wav_obj.play()
-            play_obj.wait_done()
-        else:
-            print("未発見", datetime.datetime.now())
-        time.sleep(10)
+if __name__ == '__main__':
+    url = "https://epark.jp/receive_department/wait/reserve_minute_select_ajax/172441/depart/21429?receive%5Breceipt_date%5D%5Bdate%5D=1603810800&receive%5Breceipt_date%5D%5Bhour%5D="
+    hours = [18, 19, 20]
+    mugen(base_url=url, suffix=hours)
