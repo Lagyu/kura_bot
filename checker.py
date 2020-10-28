cc6b446bc90c57d7d88c30db42880b6877f8f681
Yuya Sasaki <yuya.sasaki@2fc-group.jp>
2020-10-28 11:41:10 +0900

    initial commit

---
+++ checker.py
@@ -0,0 +1,20 @@
+import time
+import datetime
+
+import requests
+import simpleaudio
+
+base_url = "https://epark.jp/receive_department/wait/reserve_minute_select_ajax/110202/depart/11768?receive%5Breceipt_date%5D%5Bdate%5D=1603810800&receive%5Breceipt_date%5D%5Bhour%5D="
+hours = [18, 19, 20]
+
+while True:
+    for hour in hours:
+        res = requests.get(base_url + str(hour))
+        if "～" in res.text:
+            print(f"{hour}発見！")
+            wav_obj = simpleaudio.WaveObject.from_wave_file("発見の音.wav")
+            play_obj = wav_obj.play()
+            play_obj.wait_done()
+        else:
+            print("未発見", datetime.datetime.now())
+        time.sleep(10)
